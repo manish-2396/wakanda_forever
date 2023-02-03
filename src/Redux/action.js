3f5c7639b013096a73f45f30cccfeeb3bb2193ca
Manish Singh Chouhan <100896129+manish-2396@users.noreply.github.com>
2023-02-03 05:49:38 +0530

    Update action.js
    Update

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -12,6 +12,6 @@ export const postText = (payload) => (dispatch) => {
       err({ massage: "some thing went wrong" });
     }
   })
-    .then((res) => dispatch({ type: types.POST_SUCCESS, payload: TextArray }))
+    .then((res) => dispatch({ type: types.POST_SUCCESS, payload: res }))
     .catch((err) => dispatch({ type: types.POST_FAILURE }));
 };
